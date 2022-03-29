6f0a93efe12654212d72b30f1057aa255ffad517
Sam <s.blanchard.au@gmail.com>
2022-03-29 16:14:57 +1100

    Added Weather Forecast Layout

---
+++ src/WeatherForecast.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function WeatherForecast() {
+  return (
+    <div>
+      <ul>
+        <li>Sunny</li>
+        <li>25°C / 18°C</li>
+        <li>Wednesday</li>
+      </ul>
+    </div>
+  );
+}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,37 +1,41 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import WeatherForecast from "./WeatherForecast";
 
 export default function WeatherInfo(props) {
   return (
-    <div className="row">
-      <div className="col current">
-        <h1 className="cityName">{props.data.city}</h1>
-        <span className="currWeatherDetailed">
-          <ul>
-            <li>
-              <small>
-                <FormattedDate date={props.data.date} />
-              </small>
-            </li>
-            <br />
-            <li className="text-capitalize">{props.data.description}</li>
-            <li>Feels like {props.data.feels}°c</li>
-            <li>Wind: {props.data.wind} km/s</li>
-            <li>Humidity: {props.data.humidity}% </li>
-          </ul>
-        </span>
+    <div>
+      <div className="row">
+        <div className="col current">
+          <h1 className="cityName">{props.data.city}</h1>
+          <span className="currWeatherDetailed">
+            <ul>
+              <li>
+                <small>
+                  <FormattedDate date={props.data.date} />
+                </small>
+              </li>
+              <br />
+              <li className="text-capitalize">{props.data.description}</li>
+              <li>Feels like {props.data.feels}°c</li>
+              <li>Wind: {props.data.wind} km/s</li>
+              <li>Humidity: {props.data.humidity}% </li>
+            </ul>
+          </span>
+        </div>
+        <div className="col weather-temperature">
+          <img
+            src={props.data.icon}
+            id="weather-icon"
+            alt={props.data.description}
+          />
+          <h2>
+            <WeatherTemperature celsiusTemp={props.data.temperature} />
+          </h2>
+        </div>
       </div>
-      <div className="col weather-temperature">
-        <img
-          src={props.data.icon}
-          id="weather-icon"
-          alt={props.data.description}
-        />
-        <h2>
-          <WeatherTemperature celsiusTemp={props.data.temperature} />
-        </h2>
-      </div>
+      <WeatherForecast />
     </div>
   );
 }
